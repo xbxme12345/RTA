f09fef4039ab6abd4c752a1c404e284f787dbca2
Marissa Fruneaux <fruneauxm@wit.edu>
2018-03-29 09:25:22 -0400

    button fix

--- rtascript.js
+++ rtascript.js
@@ -1,26 +1,50 @@
-var tabs = $('.tabs > li');
 
-tabs.on("click", function(){
-  tabs.removeClass('active');
-  $(this).addClass('active');
-});
+
+window.onload = function(){
+	
+	var btnAddDest = document.getElementById("myAddBtn");
+	
+	//Initialize the list
+	initDest();
+	
+	btnAddDest.addEventListener("click", function(e){
+		addDest();
+	});
+	
+		// this code is not working
+	var tabs = $('.tabs > li');
+
+	tabs.on("click", function(){
+	  tabs.removeClass('active');
+	  $(this).addClass('active');
+	});
+	
+}
+
+function initDest() {
+	addDest();
+	addDest();
+}
 
 function addDest() {
-	"use strict";
-	var destinations = document.querySelectorAll(".dest");
-	var last = destinations[destinations.length-1];
-			console.log(destinations);
+	
+	var destination = document.getElementById("dest");
+	
 	var create = document.createElement("div");
+	
 	create.className = "dest";
 	var d = document.createElement("img");
 	d.className = "d";
 	d.src = "images/Dest.png";
+	
 	var del = document.createElement("img");
 	del.className = "delete";
 	del.setAttribute('src', 'images/delete.png');
+	
 	var drag = document.createElement("img");
 	drag.className = "drag";
 	drag.setAttribute('src','images/drag.png');
+	
 	var loc = document.createElement("input");
 	loc.className = "location";
 	loc.type = "text";
@@ -29,6 +53,7 @@ function addDest() {
 	create.appendChild(loc);
 	create.appendChild(del);
 	create.appendChild(drag);
-	last.parentElement.appendChild(create);
+	
+	destination.appendChild(create);
 	
 }
